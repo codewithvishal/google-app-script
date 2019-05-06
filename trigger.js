86d2038b19fa1816c2dfaeced7f291868d4c6130
Vishal Chopra <34140031+vishalchopra13@users.noreply.github.com>
2019-05-06 22:57:09 +0530

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -1,11 +1,8 @@
 // Google AppScript Trigger
-
 function Schedule (FUNCTION_NAME) {
      ScriptApp.newTrigger(FUNCTION_NAME)  // HERE IN THIS CASH PUSHBULLET
                .timeBased()
                 .everyMinutes(1)
                  .create();
  }
- 
- 
  // To delete appscript trigger, visit https://script.google.com/home/triggers
