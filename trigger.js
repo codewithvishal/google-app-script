7ed503b5fd83139781c1c1eefd7161555b53c469
Vishal Chopra <34140031+digitalvishal19@users.noreply.github.com>
2019-06-29 11:54:03 +0530

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -5,4 +5,13 @@ function Schedule (FUNCTION_NAME) {
                 .everyMinutes(1)
                  .create();
  }
- // To delete appscript trigger, visit https://script.google.com/home/triggers
+
+ // To delete appscript trigger, visit https://script.google.com/home/triggers to delete one by one or run the below function to delete all the triggers
+function StopeTriggerAlert() {
+  // Deletes all triggers in the current project.
+  var triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+  return;
+}
