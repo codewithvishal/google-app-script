4a3c6b3caf3069623d7271343018758f4ad36ca3
Vishal Chopra <34140031+digitalvishal19@users.noreply.github.com>
2019-10-28 22:28:13 +0530

    Create pushover.js

---
+++ pushover.js
@@ -0,0 +1,21 @@
+
+function Pushover() {
+//setup the Pushover API call
+var baseUrl = "https://api.pushover.net/1/messages.json";
+
+var parameters = {
+"token" : "API_KEY_HERE", //paste your API Key between the single quotation marks
+"user" : "USER_KEY_HERE", //paste your User Key between the single quotation marks
+"title" : title,
+"message" : contents2
+};
+
+var options = {
+"method" : "POST",
+"payload" : parameters
+};
+
+//send the Pushover API call
+UrlFetchApp.fetch(baseUrl,options);
+
+}
